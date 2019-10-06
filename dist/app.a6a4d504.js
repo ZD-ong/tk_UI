7e07f04d400bc6ccc7ba74cbec7a36706aabdaf2
ZD-ong <384474574@qq.com>
2019-10-06 18:19:03 +0800

    按钮添加icon

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12416,7 +12416,26 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+//
+//
+//
+//
+//
+//
+//
+var _default = {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: function validator(value) {
+        // 属性检查器
+        return value === 'right' || value === 'left';
+      }
+    }
+  }
+};
 exports.default = _default;
         var $47ee99 = exports.default || module.exports;
       
@@ -12427,10 +12446,26 @@ exports.default = _default;
         /* template */
         Object.assign($47ee99, (function () {
           var render = function() {
+  var _obj
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "tk-button" }, [_vm._v("按钮")])
+  return _c(
+    "button",
+    {
+      staticClass: "tk-button",
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+    },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon" }, [
+            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
+    ]
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12507,7 +12542,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64602" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61492" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/button.vue
+++ src/button.vue
@@ -1,13 +1,33 @@
 <template>
-    <button class="tk-button">按钮</button>
+    <button class="tk-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
 export default {
-
+    props: {
+        icon: {},
+        iconPosition: {
+            type: String,
+            default: 'left',
+            validator(value){ // 属性检查器
+                return value === 'right' || value === 'left'
+            }
+        }
+    }
 }
 </script>
 <style lang="scss">
 .tk-button {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     font-size: var(--font-size);
     height: var(--button-height);
     padding: 0 1em;
@@ -23,6 +43,23 @@ export default {
     }
     &:focus {
         outline: none;
+    }
+    > .icon {
+        order: 1;
+        margin-right: .3em;
+    }
+    > .content {
+        order: 2;
+    }
+    &.icon-right {
+        > .icon {
+            order: 2;
+            margin-right: 0;
+            margin-left: .3em;
+        }
+        > .content {
+            order: 1;
+        }
     }
 }
 </style>
