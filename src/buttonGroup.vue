1a6b5271c89ce5f63d7605bc9d984055877a9cbc
左冬 <zuodong@baijiahulian.com>
2019-11-10 16:02:53 +0800

    button-group的第一个元素不需要加margin-left: -1px

--- src/buttonGroup.vue
+++ src/buttonGroup.vue
@@ -15,7 +15,10 @@ export default {
 
     > .tk-button {
         border-radius: 0;
-        margin-left: -1px;
+
+        &:not(:first-child) {
+            margin-left: -1px;
+        }
 
         &:first-child {
             border-top-left-radius: var(--button-radius);
